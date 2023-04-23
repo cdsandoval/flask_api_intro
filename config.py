d459898106a78c3120458534991392540b796898
Carlos Sandoval Montoya <iznomag@gmail.com>
2023-04-23 21:22:53 +0200

    add: sqlalchemy and setup database

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/flask_api'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 
 config = {
