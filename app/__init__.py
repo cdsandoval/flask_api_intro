850bb9196ea7ce83fc100d0e1d34e5a1c5928ac4
Carlos Sandoval Montoya <iznomag@gmail.com>
2023-04-23 21:47:11 +0200

    add: task routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from .models import db
 from .models.task import Task
-
+from .views import app_v1
 app = Flask(__name__)
 
 
 def create_app(environment):
     app.config.from_object(environment)
+    app.register_blueprint(app_v1)
 
     with app.app_context():
         db.init_app(app)
---
+++ app/views.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, jsonify
+
+app_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
+
+
+@app_v1.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({
+        'message': 'Funciona'
+    })
+
+
+@app_v1.route('/tasks/<id>', methods=['GET'])
+def get_task():
+    pass
+
+
+@app_v1.route('/tasks', methods=['POST'])
+def create_task():
+    pass
+
+
+@app_v1.route('/tasks/<id>', methods=['PUT'])
+def update_task():
+    pass
+
+
+@app_v1.route('/tasks/<id>', methods=['DELETE'])
+def delete_task():
+    pass
