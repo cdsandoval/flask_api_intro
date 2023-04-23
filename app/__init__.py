fe2103e3aa972fb21ee55fd2269c39298eeb3c93
Carlos Sandoval Montoya <iznomag@gmail.com>
2023-04-23 21:35:54 +0200

    add: task model

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from .models import db
+from .models.task import Task
 
 app = Flask(__name__)
 
 
 def create_app(environment):
     app.config.from_object(environment)
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
     return app
